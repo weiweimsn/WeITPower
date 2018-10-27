dd8058aacbe6b6d1374edb659fe2389ff48526b5
Wei Wei <wei.wei@weitpower.com>
2018-10-27 15:27:01 -0400

    Oct27, 2018

--- slideshow/main.js
+++ slideshow/main.js
@@ -1,5 +1,13 @@
-$(function () {
-    $("#slideshow > div:gt(0)").hide();
+window.onload = function () {
+    var currentPic = document.getElementById("slideshow").children[0];
+    currentPic.style.display = "block";
+    setInterval(function () {
+        var nextPic = getNextPicture(currentPic);
+        nextPic.style.display = "block";
+        currentPic.style.display = "none";
+        currentPic = nextPic;
+    }, 3000);
+
 
     // setInterval(function () {
     //     $('#slideshow > div:first')
@@ -9,11 +17,14 @@ $(function () {
     //         .end()
     //         .appendTo('#slideshow');
     // }, 3000);
+};
 
-    $('#slideshow > div:first')
-            .fadeOut(1000)
-            .next()
-            .fadeIn(1000)
-            .end()
-            .appendTo('#slideshow');
-});
+function getNextPicture(currentPic) {
+    var pics = document.getElementById("slideshow").children;
+    for (var i = 0; i < pics.length; i++) {
+        if (currentPic.children[0].src === pics[i].children[0].src) {
+            var index = (i + 1) % 3;
+            return pics[index];
+        }
+    }
+}
