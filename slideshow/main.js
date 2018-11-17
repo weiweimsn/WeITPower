2daf197fc88c4350a1726ad4d42cec82d6aae0c6
weiweimsn <wei.wei@weitpower.com>
2018-11-16 22:06:20 -0500

    Oct 10, 2018

--- slideshow/main.js
+++ slideshow/main.js
@@ -1,3 +1,4 @@
+var currentPicIndex = 0;
 window.onload = function () {
     var currentPic = document.getElementById("slideshow").children[0];
     currentPic.style.display = "block";
@@ -27,4 +28,31 @@ function getNextPicture(currentPic) {
             return pics[index];
         }
     }
+}
+
+function getPrevPic(currentPic){
+    var pics = document.getElementById("slideshow").children;
+    for (var i = 0; i < pics.length; i++) {
+        if (currentPic.children[0].src === pics[i].children[0].src) {
+            var index = (i - 1 + 3) % 3;
+            return pics[index];
+        }
+    }
+}
+
+function goToPrevPic(){
+    var currentPic = document.getElementById("slideshow").children[currentPicIndex];
+    currentPic.style.display = "block";
+    var nextPic = getPrevPic(currentPic);
+    nextPic.style.display = "block";
+    currentPic.style.display = "none";
+    currentPicIndex = (currentPicIndex - 1 + 3) % 3;
+}
+function goToNextPic(){
+    var currentPic = document.getElementById("slideshow").children[currentPicIndex];
+    currentPic.style.display = "block";
+    var nextPic = getNextPicture(currentPic);
+    nextPic.style.display = "block";
+    currentPic.style.display = "none";
+    currentPicIndex = ++currentPicIndex % 3;
 }
