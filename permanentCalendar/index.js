28b1e68b50ab2f6494fd143940e12901ce4507b7
Wei Wei <wei.wei@weitpower.com>
2018-12-08 14:00:21 -0500

    calendar with real-time display

--- permanentCalendar/index.js
+++ permanentCalendar/index.js
@@ -16,13 +16,35 @@ function renderCalendar(){
     var seconds = date.getSeconds();
 
     var count = 1;
-    var row = 1;
-    var startIndex = row * firstDay + 6;
+    var startIndex = firstDay + 6;
+    var currentDay;
+    
     while(count <= numberOfDays){
         var calendarCell = document.getElementsByClassName("col");
+        // show time
+        var time = hour + ' : ' + minutes + ' ' + seconds;
         calendarCell[startIndex].innerHTML = count;
         calendarCell[today + 11].classList.add('today'); 
+        currentDay = calendarCell[today + 11];
         count++;
         startIndex++;
     }
+    SetTime(currentDay, today, hour, minutes,seconds);
+}
+
+function SetTime(currentDay,today, hour, minute, second){
+    setInterval(() => {
+        second ++;
+        if(second >= 60){
+            second = second % 60;
+            minute ++;
+        }
+        if(minute >= 60){
+            minute = minute % 60;
+            hour++;
+        }
+        hour = hour >= 24? hour % 24: hour;
+        var time = hour + ' : ' + minute + ' ' + second;
+        currentDay.innerHTML = today + '</br>' + '</br>' + time;
+    }, (1000));
 }
