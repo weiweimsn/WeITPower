98cf8a9077ebce80b9cad63a6ec4740d618a149a
Wei Wei <wei.wei@weitpower.com>
2019-01-12 21:37:44 -0500

    Add icons

--- permanentCalendar/index.js
+++ permanentCalendar/index.js
@@ -210,28 +210,19 @@ function setYearInfo(e) {
     holidays = updateStatHolidays(currentDate.getFullYear());
 }
 
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-
-        // Check if the XMLHttpRequest object has a "withCredentials" property.
-        // "withCredentials" only exists on XMLHTTPRequest2 objects.
-        xhr.open(method, url, true);
-
-    } else if (typeof XDomainRequest != "undefined") {
-
-        // Otherwise, check if XDomainRequest.
-        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-
-    } else {
-
-        // Otherwise, CORS is not supported by the browser.
-        xhr = null;
-
+function createCORSRequest() {
+    var url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CA";
+    var httpRequest = HttpRequest;
+    
+    httpRequest.onload = function(response){
+        console.log(response);
     }
-    return xhr;
+
+    httpRequest.onerror = function(error){
+        console.log(error);
+    }
+
+    httpRequest.send();
 }
 
 function updateStatHolidays(year) {
