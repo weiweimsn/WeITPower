1c7470d5d667d5536b8ff4ee2b615794de9ec4dd
Wei Wei <wei.wei@weitpower.com>
2019-01-19 22:56:29 -0500

    Add birthdays functionality

---
+++ permanentCalendar/libs/birthdays.js
@@ -0,0 +1,5 @@
+var Birthdays = (function(){
+    let StanleyBirthday = {date: '0110', isLunar: true, name: 'Stanley Day'};
+    let MichaelBirthday = {date: '0717', isLunar: false, name: 'Michael Day'};
+    return [StanleyBirthday, MichaelBirthday];
+})();
