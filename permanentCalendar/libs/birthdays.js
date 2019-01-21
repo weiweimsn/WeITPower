fe6f6f3520d7aac6f78adbdb2ac97a760a561870
Wei Wei <wei.wei@weitpower.com>
2019-01-20 21:51:48 -0500

    add birthday data

--- permanentCalendar/libs/birthdays.js
+++ permanentCalendar/libs/birthdays.js
@@ -1,7 +1,11 @@
 var Birthdays = (function(){
-    let StanleyBirthday = {date: '0110', isLunar: true, name: 'Stanley Day'};
-    let MichaelBirthday = {date: '0717', isLunar: false, name: 'Michael Day'};
-    return [StanleyBirthday, MichaelBirthday];
+    // let StanleyBirthday = {date: '0110', isLunar: true, name: 'Stanley Day'};
+    // let MichaelBirthday = {date: '0717', isLunar: false, name: 'Michael Day'};
+    let TangYuLanBirthday = {date: '0217', isLunar: false, name: 'Tang Yulan BirthDay'};
+    let WeiJianYongBirthday = {date: '0303', isLunar: false, name: 'Wei Jianyong BirthDay'};
+    let XieZhiQiBirthday = {date: '1124', isLunar: false, name: 'Xie Zhiqi BirthDay'};
+
+    return [XieZhiQiBirthday, TangYuLanBirthday, WeiJianYongBirthday];
 })();
 
 export default Birthdays;
