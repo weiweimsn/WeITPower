d0a3540630ebbdffd4c72ab68c075ca828802252
Wei Wei <wei.wei@weitpower.com>
2019-01-31 00:40:37 -0500

    add birth data

--- permanentCalendar/libs/birthdays.js
+++ permanentCalendar/libs/birthdays.js
@@ -1,11 +1,14 @@
 var Birthdays = (function(){
     // let StanleyBirthday = {date: '0110', isLunar: true, name: 'Stanley Day'};
     // let MichaelBirthday = {date: '0717', isLunar: false, name: 'Michael Day'};
-    let TangYuLanBirthday = {date: '0217', isLunar: false, name: 'Tang Yulan BirthDay'};
-    let WeiJianYongBirthday = {date: '0303', isLunar: false, name: 'Wei Jianyong BirthDay'};
-    let XieZhiQiBirthday = {date: '1124', isLunar: false, name: 'Xie Zhiqi BirthDay'};
+    let TangYuLanBirthday = {date: '0217', isLunar: false, name: '汤玉兰生日'};
+    let WeiJianYongBirthday = {date: '0303', isLunar: false, name: '魏建勇身日'};
+    let XieZhiQiBirthday = {date: '1124', isLunar: false, name: '谢芷淇生日'};
+    let MyMomBirthday = {date: '0223', isLunar: true, name: '我妈生日'}; // 19650325 is solar day
+    let WaiGongBirthday = {date: '0826', isLunar: true, name: '邢金权生日'}; // 1943 is the year  下午2点
+    let WaiPoBirthday = {date: '1113', isLunar: true, name: '刘路琴生日'}; // 1946 晚上11点
 
-    return [XieZhiQiBirthday, TangYuLanBirthday, WeiJianYongBirthday];
+    return [WaiGongBirthday,WaiPoBirthday, MyMomBirthday, XieZhiQiBirthday, TangYuLanBirthday, WeiJianYongBirthday];
 })();
 
 export default Birthdays;
